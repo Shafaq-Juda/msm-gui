e8523b7e7665df8e2d14300734d03fd9419c8cb4
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2024-02-15 17:33:53 +0000

    created form for directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,27 @@
 class DirectorsController < ApplicationController
+  def create
+
+    # retrieve the user's inputs from params
+    # create a record in the movie table
+    # populate each column with the user input
+    # save
+
+    # redirected the user back to the /movies url
+
+    #create new movie model instance and fetching data from the text field of form 
+    #the values afterthe_ must match the column values of director model and the value in "" () after fetch comes from the form name attributes for the text fields:
+    
+    director = Director.new
+    the_name = params.fetch("the_name")
+    the_dob = params.fetch("the_dob")
+    the_bio = params.fetch("the_bio")
+    the_image = params.fetch("the_image")
+
+    director.save
+
+    redirect_to("/directors")
+  end
+
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
@@ -36,4 +59,7 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+ 
+
 end
